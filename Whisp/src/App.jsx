13db1e6f28c784f2f75c75bfce32b81b8b2ca6a0
Yousef <yousefm2315@gmail.com>
2025-04-19 21:03:09 -0400

    created login page

--- Whisp/src/App.jsx
+++ Whisp/src/App.jsx
@@ -1,16 +1,67 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  const [currentView, setCurrentView] = useState('login')
+
+  const LoginView = () => (
+        <>
+          <h1>Whisp</h1>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button> Login</button>
+          <p>Don't have an account? <a href="#" onClick={(e) => {e.preventDefault; setCurrentView('signup')}}>Sign up</a></p>
+          <a href='#' onClick={(e) => {e.preventDefault; setCurrentView('forgotPassword')}}>Forgot Password?</a>
+        </>
+  )
+
+  const SignUpView = () => (
+  <>
+    <h1>Sign up</h1>
+
+    <div className='signupInput'>
+      <p htmlFor='username'>Username</p>
+      <input type='text' id='username' className='Username'></input>
+    </div>
+
+    <div className='signupInput'>
+      <p htmlFor='email'>Email</p>
+      <input type='text' id='email' className='Email'></input>
+    </div>
+
+    <div className='signupInput'>
+      <p htmlFor='password'>Password</p>
+      <input type='password' id='password' className='Password'></input>
+    </div>
+
+    <div className='signupInput'>
+      <p htmlFor='confirmpassword'>Confirm Password</p>
+      <input type='password' id='confirmpassword' className='confirmPassword'></input>
+    </div>
+    
+    <button>Sign Up</button>
+    <a href='#' onClick={(e) => {e.preventDefault(); setCurrentView('login')}}>Go Back</a>
+  </>
+  )
+
+  const ForgotPasswordView = () => (
+    <>
+      <h1>Reset Password</h1>
+      <p>Enter Email</p>
+      <input type='text' className=''></input>
+      <button>Send Link</button>
+      <a href='#' onClick={(e) => {e.preventDefault(); setCurrentView('login')}}>Go Back</a>
+    </>
+  )
 
   return (
     <>
       <div className="Login-container">
         <div className="Login-box">
-          <h1>Whisp</h1>
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button> Login</button>
-          <p>Don't have an account? <a href="#">Sign up</a></p>
+          {currentView === 'login' && <LoginView/>}
+          {currentView === 'signup' && <SignUpView/>}
+          {currentView === 'forgotPassword' && <ForgotPasswordView/>}
         </div>
       </div>
     </>
